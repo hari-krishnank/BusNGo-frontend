39adeb0dffb5237d91de3cd17ae575be7e77d504
Harikrishnan <hkharikrishnan23@gmail.com>
2024-10-30 21:25:38 +0530

    user interceptor changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -8,4 +8,4 @@ export const environment = {
     STRIPE_KEY: 'pk_test_51Pub5WGiScRw7LQAiwRujD55Rx8QCK5TlXwiaiC2k9EXpiJeytPZNNrtAxvOjnzPOn869YI1LPC2FFUuyCmnOtBw006J7tCTlN',
     GEMINI_API_URL: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent',    
     GEMINI_API_KEY: 'AIzaSyDbY9oumQH1JQZZIh9_WWkYZMipVeYMslI'
-}; 
+};
